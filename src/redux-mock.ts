3f73f0fefb7d9aa71255e0c6586fa8e6d8a8bcaa
rxjs-space <rxjs-space@outlook.com>
2016-10-24 20:34:29 -0700

    modifies redux-mock

--- src/redux-mock.ts
+++ src/redux-mock.ts
@@ -3,48 +3,103 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/scan';
 
 /*
-in essence:
-state$$.subscribe(console.log)
-actions$$.map(action=>newState).subscribe(state$$)
+
+action$$.next() pushes action to action$$
+this action will be mapped to a changeFn
+this changeFn will be reduced to a state
+this state will be pushed to state$$
+and state$$ pushes the state to console.log
+
 */
 
+
+/*
+  定义interfaces, types；
+  定义INCREASE, DECREASE两个常数，这样，后面可以用INCREASE变量来代替'INCREASE'字符串；
+*/
+type ActionType = 'INCREASE' | 'DECREASE'
 interface Action {
-  type: 'INCREASE' | 'DECREASE'
+  type: ActionType
 }
-interface State {
-  value: number
-}
+const INCREASE: ActionType = 'INCREASE';
+const DECREASE: ActionType = 'DECREASE';
+type State = number;
+type ChangeFn = (state: State) => State;
 
-const actions$$: Subject<Action> = new Subject()
-const state$$: BehaviorSubject<State> = new BehaviorSubject({value: 0})
-const ultimate_: Subscription = state$$.subscribe(console.log);    // {value: 0}
+/*
+ 创建action$$，负责转发action
+ 创建state$$，负责转发state，并留存最近的state
+ 立即用console.log订阅到state$$，这样避免漏数据。
+ 在console.log订阅到state$$之后，state$$会立即将留存的state推给console.log，在console上看到0。
+*/
+const action$$: Subject<Action> = new Subject()
+const state$$: BehaviorSubject<State> = new BehaviorSubject(0)
+const ultimate_: Subscription = state$$.subscribe(console.log); // will log 0 immediately
 
-const state$: Observable<State> = actions$$
+/*
+  changeFn$将action映射（map）成changeFn。
+*/
+const changeFn$: Observable<ChangeFn> = action$$
   .map((action: Action) => {
-    const state: State = Object.assign({}, state$$.getValue())
     switch (action.type) {
-      case 'INCREASE':
-        Object.assign(state, {value: state.value + 1});
-        break;
-      case 'DECREASE':
-        Object.assign(state, {value: state.value - 1});
-        break;
-      // default:
+      case INCREASE:
+        return (state: State) => state + 1;
+      case DECREASE:
+        return (state: State) => state - 1;
+      default:
+        return (state: State) => state;
     }
-    return state
   })
 
-const intermediate_ = state$.subscribe(state$$) // subscribe() launches state$ execution, state$ starts to push values to state$$
+/*
+  state$将changFn递归（scan）成state，，state初始为0。
+*/
+const state$: Observable<State> = changeFn$
+  .scan((state, changeFn) => changeFn(state), 0)
 
-actions$$.next({type: 'INCREASE'}) // {value: 1}
-actions$$.next({type: 'INCREASE'}) // {value: 2}
-actions$$.next({type: 'DECREASE'}) // {value: 1}
+/*
+  用state$$订阅state$，state$开始向state$$推送数据
+*/
+const intermediate_: Subscription = state$.subscribe(state$$) 
 
-console.log(state$$.getValue())    // current state is {value: 1}
+/*
+  用action$$.next()来向action$$推送action；
+  这个action会经过map，scan变成一个state，然后被推给state$$
+*/
+action$$.next({type: INCREASE}) // 1
+action$$.next({type: INCREASE}) // 2
+action$$.next({type: DECREASE}) // 1
+
+/*
+  获取最近的state，用BehaviorSubject.getValue()
+*/
+console.log(state$$.getValue())    // current state is 1
+
+// 在页面上创建两个按钮
+const section = document.createElement('section')
+document.body.appendChild(section);
+const incButton = document.createElement('button');
+incButton.innerText = INCREASE;
+section.appendChild(incButton);
+const decButton = document.createElement('button');
+decButton.innerText = DECREASE;
+section.appendChild(decButton);
+
+
+/*
+  创建click事件Observable，每次点按会推送一个action给action$$。
+  立即调用subscribe()，以启动Observable，这里不必制定任何observer。
+*/
+Observable.fromEvent(incButton, 'click')
+  .map(() => action$$.next({type: INCREASE})).subscribe()
+Observable.fromEvent(decButton, 'click')
+  .map(() => action$$.next({type: DECREASE})).subscribe()
+
 
 /*
 
